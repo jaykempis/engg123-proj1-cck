20379e46d6f409e6204fc781c048f3c80d2d93db
jaykempis <jay.kempis@bolt.graphics>
2022-09-26 22:50:52 +0800

    updated files

--- hexTest_R.cpp
+++ hexTest_R.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main()
 {
+    long long int reg[64];
     string s = "0x007302B3";
     stringstream ss;
 
@@ -17,6 +18,7 @@ int main()
     bitset<32> b(n); //raw 32-bit line
     cout << b.to_string() << endl;
 
+    //extraction
     unsigned opcode = n & 0x0000007F; //extracted OP-code
     unsigned r_dest = (n & 0x00000F80) >> 7; //extracted rd
     unsigned funct_3 = (n & 0x00007000) >> 12; //extracted f3
@@ -24,8 +26,7 @@ int main()
     unsigned r_src2 = (n & 0x01F00000) >> 20; //extracted rs1
     unsigned funct_7 = (n & 0xFE000000) >> 25; //extracted rs1
 
-
-
+    //fields
     bitset<7> op(opcode);
     bitset<5> rd(r_dest);
     bitset<3> f3(funct_3);
@@ -40,5 +41,29 @@ int main()
     cout << rs1.to_string() << endl;
     cout << rs2.to_string() << endl;
     cout << f7.to_string() << endl;
+
+    //storing values in memory
+    reg[rs1.to_ulong()] = 46;
+    reg[rs2.to_ulong()] = 23;
+
+    //sample orperation
+    switch(f3.to_ulong()){
+        case 0b000:
+            if(f7.to_ulong() == 0b0000000){
+                reg[rd.to_ulong()] = reg[rs1.to_ulong()] + reg[rs2.to_ulong()];
+            }
+            else if(f7.to_ulong() == 0b0100000){
+                reg[rd.to_ulong()] = reg[rs1.to_ulong()] - reg[rs2.to_ulong()];
+            }
+
+            cout << reg[rd.to_ulong()] << endl;
+        break;
+
+        default:
+            cout << "error";
+        break;
+    }
+
+
 
 }
