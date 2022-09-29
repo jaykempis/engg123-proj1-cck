682bb6b0b5e35f796412b091220c6b15ab2795ae
jaykempis <jay.kempis@bolt.graphics>
2022-09-29 20:46:42 +0800

    Added hexTest_I
    sample code structure for Immediate Instructions

---
+++ hexTest_I.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<sstream>
+#include<bitset>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    long long int reg[64];
+    string s = "0xFCE08793";
+    stringstream ss;  
+
+    ss << hex << s;
+    unsigned n;
+    ss >> n;
+
+    bitset<32> b(n); //raw 32-bit line
+    cout << b.to_string() << endl;
+
+    //extraction
+    unsigned opcode = n & 0x0000007F; //extracted OP-code
+    unsigned r_dest = (n & 0x00000F80) >> 7; //extracted rd
+    unsigned funct_3 = (n & 0x00007000) >> 12; //extracted f3
+    unsigned r_src1 = (n & 0x000F8000) >> 15; //extracted rs1
+    unsigned immediate = (n & 0xFFF00000) >> 20; //extracted imm
+
+    //fields
+    bitset<7> op(opcode);
+    bitset<5> rd(r_dest);
+    bitset<3> f3(funct_3);
+    bitset<5> rs1(r_src1);
+    bitset<12> imm(immediate);
+
+    //outputs
+    cout << op.to_string() << endl;
+    cout << rd.to_string() << endl;
+    cout << f3.to_string() << endl;
+    cout << rs1.to_string() << endl;
+    cout << imm.to_string() << endl;
+
+    //storing values in memory
+    reg[rs1.to_ulong()] = 46;
+    int16_t x_imm = (int16_t)(imm.to_ulong() & 0xFFF) << 4;
+    x_imm = x_imm >> 4;
+    bitset<12> test_imm(x_imm);
+    cout << test_imm.to_string() << endl;
+
+    //sample orperation
+    switch(f3.to_ulong()){
+        case 0b000: //ADD I
+            cout << "signed value: " << x_imm << endl;
+            reg[rd.to_ulong()] = reg[rs1.to_ulong()] + x_imm;
+            cout << reg[rd.to_ulong()] << endl;
+            cout << "PSEUDO addi R" << rd.to_ulong() <<
+          " , R" <<rs1.to_ulong() << " , " << x_imm << "   |   R" << rd.to_ulong() << ": ";
+            cout << reg[rd.to_ulong()] << endl;
+        break;
+
+        default:
+            cout << "error";
+        break;
+    }
+
+
+
+}
